688072b002a1c9877cbc584c003c91f32d2e5244
xiaomeng xu <xiaomengxu@berkeley.edu>
2023-06-18 11:07:19 -0700

    no background

--- js/level.js
+++ js/level.js
@@ -1,16 +1,19 @@
-// JavaScript code for level.js
+const dynamicTextElement = document.getElementById('dynamicText');
+const changeButton = document.getElementById('changeButton');
 
-const words = ['A', 'B', 'C', 'D', 'E'];
-let currentWordIndex = 0;
+const texts = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E'
+];
 
-function displayText() {
-  const textContainer = document.getElementById('dynamicText');
-  textContainer.textContent = words[currentWordIndex];
+let currentIndex = 0;
 
-  if (currentWordIndex === words.length - 1) {
-    document.getElementById('changeButton').disabled = true;
-    document.getElementById('nextButton').classList.remove('hidden');
-  }
-
-  currentWordIndex++;
+function changeText() {
+  dynamicTextElement.textContent = texts[currentIndex];
+  currentIndex = (currentIndex + 1) % texts.length;
 }
+
+changeButton.addEventListener('click', changeText);
